bebc5df191f5a6aee65e004a823c246af8e69fe6
guilhermegouw <guilherme.gouw@gmail.com>
2022-03-16 07:53:58 -0300

    Persiste a inscrição

--- eventex/settings.py
+++ eventex/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # 3rd
+    "test_without_migrations",
     "django_extensions",
     # Local
     "subscriptions",
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from subscriptions.forms import SubscriptionForm
+from subscriptions.models import Subscription
 
 
 def subscribe(request):
@@ -27,6 +28,8 @@ def create(request):
         "subscriptions/subscription_email.txt",
         form.cleaned_data,
     )
+
+    Subscription.objects.create(**form.cleaned_data)
 
     messages.success(request, "Inscrição realizada com succeso!")
 
